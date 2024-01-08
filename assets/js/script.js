c8fbb8bc5c8d509ee5a6dc40d68621c2827df186
Alexandru Pandelea <alexandrupandelea@Alexandrus-MacBook-Pro.local>
2024-01-08 18:09:14 +0000

    add values into local storage on save button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,28 @@ const currentDayEl = $('#currentDay');
 
 //Header
 //display current day
-
+function displaytime() {
 const currentDay = dayjs();
 currentDayEl.text(currentDay);
+}
+
+setInterval(displaytime(), 1000);
 
 //Table
 //based on time change the colors of the textarea rows
- function updateHoursChange() {
+function updateHoursChange() {
     //store current hour in a variable so we can compare with the id of the textarea
     var currentTime = dayjs().format('H');
     //loop through the textarea elements to check them one by one with the current time
     for (let i = 0; i < textEl.length; i++) {
-
         //get element id as a string
         var elementID = textEl[i].id;
         //get element by id
         var manipulateID = document.getElementById(elementID);
         //remove any old classes
         $(elementID).removeClass(".present .past .future");
-
         //apply new class
-
-        if(elementID < currentTime) {
+        if (elementID < currentTime) {
             $(manipulateID).addClass('past');
         } else if (elementID > currentTime) {
             $(manipulateID).addClass('future');
@@ -34,11 +34,24 @@ currentDayEl.text(currentDay);
         }
     }
 
- }
-
+}
 //Take value and save it to local storage, display in the text area
-saveBtn.on('click', function () {
-    console.log(textEl.value);
+//loop through buttons and if the name matches the id of the text area put it into storage
+saveBtn.each(function(btn) {
+    saveBtn[btn].addEventListener('click', function() {
+        for (let i = 0; i < textEl.length; i++) {
+        if(saveBtn[btn].getAttribute('name') === textEl[i].id) {
+            const savedDataArr = JSON.parse(localStorage.getItem('data')) || [];
+            const data = {
+                hour: textEl[i].id,
+                text: textEl[i].value.trim()
+            };
+            savedDataArr.push(data);
+            localStorage.setItem('data', JSON.stringify(savedDataArr));
+        }
+        }
+    })
 })
-
-updateHoursChange();
+ 
+//update colours every hour
+setInterval(updateHoursChange(), (1000 * 60) * 60);
